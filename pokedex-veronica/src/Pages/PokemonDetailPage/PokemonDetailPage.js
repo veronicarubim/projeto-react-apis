5abfe682a1324b4bf2b4eed246819ca937ae47fa
veronicarubim <veronica.rubim97@gmail.com>
2022-12-26 15:10:29 -0300

    Semana 1 - Criando as pastas e arquivos

---
+++ pokedex-veronica/src/Pages/PokemonDetailPage/PokemonDetailPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../../components/Header/Header'
+import PokemonCard from '../../components/PokemonCard/PokemonCard'
+
+const PokemonDetailPage = () => {
+  return (
+    <div>
+        <h1> Pokemon Detail Page</h1>
+        <Header/>
+        <PokemonCard/>
+    </div>
+  )
+}
+
+export default PokemonDetailPage
---
+++ pokedex-veronica/src/Pages/PokemonList/PokemonListStyle.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PokemonListStyle = () => {
+  return (
+    <div>PokemonListStyle</div>
+  )
+}
+
+export default PokemonListStyle
